4b528ab2869ec224287b2a2598b0dce545e3e4f8
MrBoogie27 <37020936+MrBoogie27@users.noreply.github.com>
2018-03-03 20:18:38 +0500

    Add files via upload

---
+++ CircleThread.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+namespace OC6
+{
+    public class RenderThread
+    {
+        string _priority;
+        PictureBox _childForm; //для взятия из формы
+        Thread _thread = null; //поток
+        public Color _color = new Color();//цвет
+        Brush _brush = null;
+        int _radius; //радиус
+        int _sleepDuration; //время задержки между рисованием
+        private Control _parent; //элемент для размеров окна
+        Random _random = new Random(); //для рандомного положения
+        public string _name; //имя для поиска и вывода
+        Size _parentSize; //размер окна
+        public readonly ManualResetEvent _pauseEvent; //для приостановки потока
+        public readonly ManualResetEvent _exitEvent; //для закрытия поток
+        readonly object _clientSizeChangeLocker; //для создания КС
+        public RenderThread(PictureBox childForm, Control parent, Thread thread, Color color, int radius, int i, int sleepDuration)
+        {
+            _priority = "Нормальный";
+            _sleepDuration = sleepDuration;
+            _childForm = childForm;
+            _parent = parent;
+            _thread = thread;
+            _color = color;
+            _brush= new SolidBrush(_color);
+            _radius = radius;
+            _name = "Thread" + i;
+            _parentSize = _parent.ClientSize;
+            _exitEvent = new ManualResetEvent(false);
+            _pauseEvent = new ManualResetEvent(false);
+            _clientSizeChangeLocker = new object();
+            _thread = new Thread(this.DrawCircle) { Name = _name };
+            _thread.Priority = ThreadPriority.Normal;
+        }
+        public void ParentResize(object sender, EventArgs e)
+        {
+            _parentSize = (sender as Control).ClientSize;
+            _childForm.ClientSize = new Size(_parentSize.Width-200, _parentSize.Height-10);
+        }
+        public string Priority
+        {
+            get { return _priority; }
+            set {
+                _priority = value;
+                switch (_priority)
+                {
+                    case "Высокий": _thread.Priority = ThreadPriority.Highest;break;
+                    case "Выше среднего": _thread.Priority = ThreadPriority.AboveNormal; break;
+                    case "Нормальный": _thread.Priority = ThreadPriority.Normal; break;
+                    case "Ниже среднего": _thread.Priority = ThreadPriority.BelowNormal; break;
+                    case "Низкий": _thread.Priority = ThreadPriority.Lowest; break;
+                }
+                }
+        }
+        public int SleepDuration
+        { get { return _sleepDuration; } set { _sleepDuration = value; } }
+        public int Radius
+        { get { return _radius; } set { _radius = value; } }
+        public Color UsedColor
+        { get { return _color; }
+          set {
+                lock (_clientSizeChangeLocker)
+                {
+                    _color = value;
+                    Brush k = _brush;
+                    _brush = new SolidBrush(_color);
+                    if(k != null)
+                        k.Dispose();
+
+                }
+              }
+        }
+        public void Suspend()
+        {
+            _pauseEvent.Set();
+        }
+        public void Resume()
+        {
+            _pauseEvent.Reset();
+        }
+        public void Run()
+        {
+            _thread.Start();
+        }
+        public void Dispose()
+        {
+            _exitEvent.Set();
+            Resume();
+            _thread.Join();
+            _exitEvent.Close();
+        }
+        public void DrawCircle()
+        {
+          
+            do
+            {
+                lock (_clientSizeChangeLocker)
+                    for (int i = 0; i < 100; i++)
+                {
+                    Point point = new Point { X = _random.Next(0, _childForm.ClientSize.Width - _radius), Y = _random.Next(0, _childForm.ClientSize.Height - _radius) };
+
+                    Graphics g = _childForm.CreateGraphics();
+                    g.FillEllipse(_brush, point.X, point.Y, _radius, _radius);
+                    g.Dispose();
+                }
+                if (!_thread.IsAlive)
+                    break;
+               // Thread.Sleep(0);
+                while (_pauseEvent.WaitOne(0)) ;
+            }
+            while (!_exitEvent.WaitOne(_sleepDuration));
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OC6
+{
+    public partial class Form1 : Form
+    {
+        Process thisProc;
+        int i = 1;
+        List<Thread> TList = new List<Thread>();
+        List<RenderThread> RList = new List<RenderThread>();
+        public Form1()
+        {
+            thisProc = Process.GetCurrentProcess();
+            InitializeComponent();
+            Color.Enabled = false;
+            Suspend.Enabled = false;
+            Resume.Enabled = false;
+            RemoveThread.Enabled = false;
+            Radius.Enabled = false;
+            RefreshInterval.Enabled = false;
+            Priority.Items.Add("Высокий");
+            Priority.Items.Add("Выше среднего");
+            Priority.Items.Add("Нормальный");
+            Priority.Items.Add("Ниже среднего");
+            Priority.Items.Add("Низкий");
+            Priority.Enabled = false;
+            MainPriority.Items.Add("High");
+            MainPriority.Items.Add("AboveNormal");
+            MainPriority.Items.Add("Normal");
+            MainPriority.Items.Add("BelowNormal");
+            MainPriority.Items.Add("Idle");
+            MainPriority.Items.Add("RealTime");
+            switch (thisProc.PriorityClass.ToString())
+            {
+                case "High" : MainPriority.SelectedItem = "High"; break;
+                case "AboveNormal": MainPriority.SelectedItem = "AboveNormal"; break;
+                case "Normal": MainPriority.SelectedItem = "Normal"; break;
+                case "BelowNormal": MainPriority.SelectedItem = "BelowNormal"; break;
+                case "Idle": MainPriority.SelectedItem = "Idle"; break;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread = null;
+            RenderThread render = new RenderThread(this.pictureBox1, this, thread, colorDialog1.Color, 0, i++, 0);
+            TList.Add(thread);
+            RList.Add(render);
+            ListThread.Items.Add(render._name);
+            render.Run();
+            render = null;
+        }
+        private void RemoveThread_Click(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+            {
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                {
+                    ListThread.Items.Remove(ab._name);
+                    ab.Dispose();
+                    i--;
+                    RList.Remove(ab);
+                    Color.Enabled = false;
+                    Suspend.Enabled = false;
+                    Resume.Enabled = false;
+                    RemoveThread.Enabled = false;
+                    Radius.Enabled = false;
+                    RefreshInterval.Enabled = false;
+                    Priority.Enabled = false;
+                    break;
+                }
+            }
+        }
+        private void Color_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                {
+                    ab.UsedColor = colorDialog1.Color;
+                }
+        }
+        private void Resume_Click(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                {
+                    ab.Resume();
+                    Resume.Enabled = false;
+                    Suspend.Enabled = true;
+                }
+        }
+        private void Suspend_Click(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                {
+                    ab.Suspend();
+                    Resume.Enabled = true;
+                    Suspend.Enabled = false;
+                }
+        }
+        private void Radius_ValueChanged(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                    ab.Radius = Convert.ToInt32(Radius.Value);
+        }
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (RList.Count != 0)
+            {
+                foreach (RenderThread ab in RList)
+                {
+                    ListThread.Items.Remove(ab._name);
+                    ab.Dispose();
+                    i--;
+                }
+            }
+        }
+        private void RefreshInterval_ValueChanged(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                    ab.SleepDuration = Convert.ToInt32(RefreshInterval.Value);
+        }
+        private void ListThread_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                {
+                    colorDialog1.Color = ab._color;
+                    Color.Enabled = true;
+                    RemoveThread.Enabled = true;
+                    Priority.Enabled = true;
+                    Priority.SelectedItem = ab.Priority;
+                    if (ab._pauseEvent.WaitOne(0))
+                    {
+                        Resume.Enabled = true;
+                        Suspend.Enabled = false;
+                    }
+                    else
+                    {
+                        Resume.Enabled = false;
+                        Suspend.Enabled = true;
+                    }
+                    Radius.Enabled = true;
+                    Radius.Value = ab.Radius;
+                    RefreshInterval.Enabled = true;
+                    RefreshInterval.Value = ab.SleepDuration;
+                }
+        }
+        private void Priority_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                if (ListThread.SelectedIndex != -1 && ab._name == ListThread.SelectedItem.ToString())
+                    ab.Priority = Priority.SelectedItem.ToString();
+        }
+        private void MainPriority_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (MainPriority.SelectedItem.ToString())
+            {
+                case "High":
+                    thisProc.PriorityClass = ProcessPriorityClass.High; break;
+                case "AboveNormal":
+                    thisProc.PriorityClass = ProcessPriorityClass.AboveNormal; break;
+                case "Normal":
+                    thisProc.PriorityClass = ProcessPriorityClass.Normal; break;
+                case "BelowNormal":
+                    thisProc.PriorityClass = ProcessPriorityClass.BelowNormal; break;
+                case "Idle":
+                    thisProc.PriorityClass = ProcessPriorityClass.Idle; break;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            foreach (RenderThread ab in RList)
+                ab.ParentResize(sender, e);
+        }
+    }
+}
